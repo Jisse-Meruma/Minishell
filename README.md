fdcac66d2f8308e9ab63ad163af90419ef0cb0d5
Maximilien <bernede.maximilien@gmail.com>
2023-03-29 16:20:52 +0200

    added something :)

--- README.md
+++
@@ -1,3 +0,0 @@
-# Minishell
-Minishell is a project to recreate the bash shell in the unix_terminal written in c
-Hello bro
--- src/main.c
+++ src/main.c
@@ -1,4 +1,28 @@
 #include "minishell.h"
+#define CHILD 0
+
+void execute_img(t_infos *infos) {
+    char *path;
+    infos->pid = fork();
+    if (infos->pid == -1) {
+        printf("fork Error\n");
+        return;
+    }
+    if (infos->pid == CHILD) {
+        path = ft_strdup("./imgcat");
+        if (path) {
+            char *args[] = {"imgcat", "download.png", NULL};
+            execve(path, args, NULL);
+            // execve only returns if there's an error
+            printf("execve error\n");
+        }
+        else {
+            printf("ft_strdup error\n");
+        }
+        return;
+    }
+    waitpid(infos->pid, NULL, 0);
+}
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -8,6 +32,7 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (init(&infos, envp))
 		ret_error("Error init\n", 1, 1);
+	execute_img(&infos);
 	line = readline("\x1b[1m\x1b[38;2;0;255;255mCeleste-shell$ \x1b[0m");
 	while (line)
 	{
