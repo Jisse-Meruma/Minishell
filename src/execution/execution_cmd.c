b85b2eb799a30600c19314b070148dd28186802d
Jisse Meruma <jissemeruma@gmail.com>
2023-09-01 20:27:59 +0200

    Done writing dup function in execution_cmd.c next testinggit add --all!

--- src/execution/execution_cmd.c
+++ src/execution/execution_cmd.c
@@ -1,5 +1,55 @@
 #include "minishell.h"
 #include <fcntl.h>
+#define FILE 1
+#define NO_FILE 0
+
+int	check_read_priority(t_command *command)
+{
+	int	priority;
+	t_lst_redirects	*redirect;
+
+	priority = 0;
+	redirect = command->lst_redirects;
+	while (redirect)
+	{
+		if (redirect->token == HERE_DOC)
+			priority = 1;
+		if (redirect->token == STDINN_FILE)
+			priority = 2;
+		redirect = redirect->next;
+	}
+	return (priority);
+}
+
+bool	dup_read(t_command *command, t_infos *infos, int check)
+{
+	close(infos->pipes[0]);
+	if (check == FILE || command->order != ONE_CMD)
+	{
+		if (dup2(infos->read_fd, STDIN_FILENO) == -1)
+			return (close(infos->read_fd), false);
+		close(infos->read_fd);
+	}
+	return (true);
+}
+
+bool	dup_write(t_command *command, t_infos *infos, int check)
+{
+	if (check == FILE)
+	{
+		if (dup2(infos->write_fd, STDOUT_FILENO) == -1)
+			return (close(infos->write_fd), false);
+		close(infos->write_fd);
+	}
+	else if (command->order != LAST_CMD && command->order != ONE_CMD)
+	{
+		if (dup2(infos->pipes[1], STDOUT_FILENO) == -1)
+			return (close(infos->write_fd), false);
+	}
+	if (command->order != LAST_CMD && command->order != ONE_CMD)
+		close(infos->pipes[1]);
+	return (true);
+}
 
 bool	exec_heredoc(char *end_of_file, t_infos *infos, int *fd)
 {
@@ -9,12 +59,12 @@ bool	exec_heredoc(char *end_of_file, t_infos *infos, int *fd)
 	return (true);
 }
 
-bool	start_heredoc(t_command *command, t_infos *infos, int *fd)
+bool	start_heredoc(t_command *command, t_infos *infos, int *check, int priority)
 {
 	int file_disciptor;
 	t_lst_redirects	*redirect;
 
-	file_disciptor == -2;
+	file_disciptor = -2;
 	redirect = command->lst_redirects;
 	while (redirect)
 	{
@@ -25,15 +75,19 @@ bool	start_heredoc(t_command *command, t_infos *infos, int *fd)
 		}
 		if (file_disciptor != -2)
 			close(file_disciptor);
+		if (file_disciptor == -2 && command->order <= 1)
+			close(infos->read_fd);
 		if (!exec_heredoc(redirect->filename, infos, &file_disciptor))
 			return (false);
+		*check = FILE;
 		redirect = redirect->next;
 	}
-	*fd = file_disciptor;
+	if (priority == 1)
+		infos->read_fd = file_disciptor;
 	return (true);
 }
 
-bool	start_read(t_command *command, t_infos *infos, int *fd)
+bool	start_read(t_command *command, t_infos *infos, int *check, int priority)
 {
 	int file_disciptor;
 	t_lst_redirects	*redirect;
@@ -49,18 +103,19 @@ bool	start_read(t_command *command, t_infos *infos, int *fd)
 		}
 		if (file_disciptor != -2)
 			close(file_disciptor);
-		if (file_disciptor == -2 && command->order <= 1)
+		if (file_disciptor == -2 && command->order <= 1 && *check)
 			close(infos->read_fd);
 		file_disciptor = open(redirect->filename, O_RDONLY);
 		if (file_disciptor == -1)
 			return (false);
 		redirect = redirect->next;
 	}
-	*fd = file_disciptor;
+	if (priority == 2)
+		infos->read_fd = file_disciptor;
 	return (true);
 }
 
-bool	start_write(t_command *command, t_infos *infos, int *fd)
+bool	start_write(t_command *command, t_infos *infos, int *check)
 {
 	int file_disciptor;
 	t_lst_redirects	*redirect;
@@ -76,33 +131,38 @@ bool	start_write(t_command *command, t_infos *infos, int *fd)
 		}
 		if (file_disciptor != -2)
 			close(file_disciptor);
-		if (file_disciptor == -2 && command->order <= 1)
-			close(infos->read_fd);
 		if (redirect->token == STDOUT_FILE)
 			file_disciptor = open(redirect->filename, O_WRONLY | O_CREAT | O_TRUNC, 0000644);
 		else if (redirect->token == APPEND_FILE)
 			file_disciptor = open(redirect->filename, O_WRONLY | O_APPEND | O_CREAT, 0000644);
 		if (file_disciptor == -1)
 			return (false);
+		*check = FILE;
 		redirect = redirect->next;
 	}
-	*fd = file_disciptor;
+	if (*check == FILE)
+		infos->write_fd = file_disciptor;
 	return (true);
 }
 
 bool dup_redirects(t_command *command, t_infos *infos)
 {
-	int				fd;
-	t_lst_redirects	*redirect;
+	int	priority;
+	int	check_read;
+	int check_write;
 	
-	if (!start_heredoc(command, infos, &infos->read_fd))
+	check_read = NO_FILE;
+	check_write = NO_FILE;
+	priority = check_read_priority(command);
+	if (!start_heredoc(command, infos, &check_read, priority))
 		return (false);
-	if (!start_read(command, infos, &infos->read_fd))
+	if (!start_read(command, infos, &check_read, priority))
 		return (false);
-	if (!start_write(command, infos, &infos->write_fd))
-
-	
-	
-
-
+	if (!start_write(command, infos, &check_write))
+		return (false);
+	if (!dup_read(command, infos, check_read))
+		return (false);
+	if (!dup_write(command, infos, check_write))
+		return (false);
+	return (true);
 }
