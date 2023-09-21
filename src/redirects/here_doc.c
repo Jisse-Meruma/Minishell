f9d98daf3db006b9281bcc2a36e78f80a8f6cb90
Jisse Meruma <jissemeruma@gmail.com>
2023-09-21 12:30:40 +0200

    H_ERROR_H

--- src/redirects/here_doc.c
+++ src/redirects/here_doc.c
@@ -1,6 +1,4 @@
 #include "minishell.h"
-#include "h_color.h"
-
 #define INVALID -1
 
 int	here_doc(char *end_of_file, t_infos *infos)
@@ -12,7 +10,7 @@ int	here_doc(char *end_of_file, t_infos *infos)
 	fd = open_here_doc(end_of_file, &path);
 	if (fd == INVALID)
 		return (INVALID);
-	line = readline("\x1b[1m\x1b[38;2;128;0;128mBadeline> \x1b[0m");
+	line = readline("Badeline>");
 	while (line)
 	{
 		if (!ft_strncmp(line, end_of_file, ft_strlen(end_of_file) + 1))
@@ -25,7 +23,7 @@ int	here_doc(char *end_of_file, t_infos *infos)
 		write(fd, line, ft_strlen(line));
 		write(fd, "\n", 1);
 		free(line);
-		line = readline("\x1b[1m\x1b[38;2;128;0;128mBadeline> \x1b[0m");
+		line = readline("Badeline>");
 	}
 	return (here_doc_unlink_and_close(path, fd));
 }
--- src/utils/is_invalid_id.c
+++ src/utils/is_invalid_id.c
@@ -2,62 +2,62 @@
 
 int ft_atoi64_overflow(const char *str, int64_t *numb)
 {
-    int i;
-    int sign;
+	int i;
+	int sign;
 
-    i = 0;
-    sign = 1;
-    *numb = 0;
-    if (!str)
-        return 1;
-    while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
-           || str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
-        i++;
-    if (str[i] == '-') 
-    {
-        sign = -1;
-        i++;
-    } 
-    else if (str[i] == '+')
-        i++;
-    while (str[i] >= '0' && str[i] <= '9') 
-    {
-        int digit = str[i] - '0';
-        if (*numb > INT64_MAX / 10 || (*numb == INT64_MAX / 10 && digit > INT64_MAX % 10))
-        {
-            if (sign == 1) 
-                *numb = INT64_MAX;
-            else
-                *numb = INT64_MIN;
-            return 1;
-        }
-        *numb = *numb * 10 + digit;
-        i++;
-    }
-    *numb *= sign;
-    return (0);
+	i = 0;
+	sign = 1;
+	*numb = 0;
+	if (!str)
+		return 1;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		   || str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	if (str[i] == '-') 
+	{
+		sign = -1;
+		i++;
+	} 
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9') 
+	{
+		int digit = str[i] - '0';
+		if (*numb > INT64_MAX / 10 || (*numb == INT64_MAX / 10 && digit > INT64_MAX % 10))
+		{
+			if (sign == 1) 
+				*numb = INT64_MAX;
+			else
+				*numb = INT64_MIN;
+			return (1);
+		}
+		*numb = *numb * 10 + digit;
+		i++;
+	}
+	*numb *= sign;
+	return (0);
 }
 
 int check_valid_id(char *str){
-    int i;
+	int i;
 
-    i = 0;
+	i = 0;
 	if (!str[0])
 		return (0);
 	if (ft_isdigit(str[0])|| str[0] == '=')
 		return (0);
-    while (str[i]){
-        if (ft_isspace(str[i]) || \
-        (str[i] >= 32 && str[i] <= 47) || \
-        (str[i] >= 58 && str[i] <= 60) || \
-        (str[i] >= 62 && str[i] <= 64) || \
-        (str[i] >= 91 && str[i] <= 94) || \
-        (str[i] == 96) || \
-        (str[i] >= 123 && str[i] <= 126))
-            return (0);
-        if (str[i] == '=')
-            return (1);
-        ++i;
-    }
+	while (str[i]){
+		if (ft_isspace(str[i]) || \
+		(str[i] >= 32 && str[i] <= 47) || \
+		(str[i] >= 58 && str[i] <= 60) || \
+		(str[i] >= 62 && str[i] <= 64) || \
+		(str[i] >= 91 && str[i] <= 94) || \
+		(str[i] == 96) || \
+		(str[i] >= 123 && str[i] <= 126))
+			return (0);
+		if (str[i] == '=')
+			return (1);
+		++i;
+	}
 	return (1);
 }
